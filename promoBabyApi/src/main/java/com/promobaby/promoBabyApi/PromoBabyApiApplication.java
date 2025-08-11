c8537db2dd00b6898eaedd8e33eef18a66a9d3c4
Pedro Monteiro <pedro.msilva@dataprev.gov.br>
2025-08-11 17:09:13 -0300

    criando projeto spring

---
+++ promoBabyApi/src/main/java/com/promobaby/promoBabyApi/PromoBabyApiApplication.java
@@ -0,0 +1,13 @@
+package com.promobaby.promoBabyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoBabyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoBabyApiApplication.class, args);
+	}
+
+}
---
+++ promoBabyApi/src/test/java/com/promobaby/promoBabyApi/PromoBabyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promobaby.promoBabyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoBabyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
